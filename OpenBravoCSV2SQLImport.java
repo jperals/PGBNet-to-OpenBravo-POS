034825cfec4f97d14001371254d1b45f065c2159
Joan Perals Tresserra <j.pertres@gmail.com>
2013-11-25 11:07:50 +0100

    Process products without valid barcode after the others

--- OpenBravoCSV2SQLImport.java
+++ OpenBravoCSV2SQLImport.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -17,7 +18,7 @@ public class OpenBravoCSV2SQLImport
     private static int productCount = 0;
     private static int productsAdded = 0;
     private static int productsSkipped = 0;
-    private static int productsWithoutCode = 0;
+    private static ArrayList<String[]> productsWithoutCode = new ArrayList<String[]>();
 
     public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
         connectAndStart();
@@ -51,7 +52,7 @@ public class OpenBravoCSV2SQLImport
             System.out.println("Products added: " + productsAdded);
             System.out.println("Products skipped: " + productsSkipped);
             System.out.println("Total: " + productCount);
-            System.out.println(productsWithoutCode + " products had a barcode full of zeros, and an incremental number was used instead.");
+            System.out.println(productsWithoutCode.size() + " products had a barcode full of zeros, and an incremental number was used instead.");
         }
     }
     
@@ -62,43 +63,58 @@ public class OpenBravoCSV2SQLImport
             while ((line = reader.readNext()) != null) {
                 insertProduct(line);
             }
+            int nProductsWithoutCode = productsWithoutCode.size();
+            System.out.println("Finished inserting products with valid barcode. Now inserting " + nProductsWithoutCode + " products which barcode was \"0000000000000\".");
+            for (int i=0; i < nProductsWithoutCode; i++) {
+                String[] productProperties = productsWithoutCode.get(i);
+                productProperties[26] = String.format("%013d", i);
+                insertProduct(productProperties);
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+        
     private static void insertProduct(String[] productProperties) throws SQLException {
-        String productId = UUID.randomUUID().toString();
-        String productName = productProperties[25].replace("\"", "\\\"");
-        int priceBuy = (Integer.parseInt(productProperties[27])/100);
-        int priceSell = (Integer.parseInt(productProperties[30])/100);
         String barcode = productProperties[26];
-        if(barcode.equals("0000000000000")) {
-            barcode = String.format("%013d", productsWithoutCode);
-            productsWithoutCode++;
+        if(barcode.equals("0000000000000") || barcode.equals("") {
+            productsWithoutCode.add(productProperties);
         }
-        String SQLQuery = "INSERT INTO PRODUCTS (ID, REFERENCE, CODE, NAME, PRICEBUY, PRICESELL, CATEGORY, TAXCAT) VALUES (\"" + productId + "\", \"" + productProperties[24] + "\", \"" + barcode + "\", \"" + productName + "\", " + priceBuy + ", " + priceSell + ", \"000\", \"001\")";
-        try {
-            rs = stmt.executeUpdate(SQLQuery);
-            if (rs > 0) {
-                productsAdded++;
-                System.out.println("Product " + productCount + " added: \"" + productProperties[25] + "\"");
+        else {
+            String productId = UUID.randomUUID().toString();
+            String productName = productProperties[25].replace("\"", "\\\"");
+            int priceBuy = (Integer.parseInt(productProperties[27])/100);
+            int priceSell = (Integer.parseInt(productProperties[30])/100);
+            String SQLQuery = "INSERT INTO PRODUCTS (ID, REFERENCE, CODE, NAME, PRICEBUY, PRICESELL, CATEGORY, TAXCAT) VALUES (\"" + productId + "\", \"" + productProperties[24] + "\", \"" + barcode + "\", \"" + productName + "\", " + priceBuy + ", " + priceSell + ", \"000\", \"001\")";
+            try {
+                rs = stmt.executeUpdate(SQLQuery);
+                if (rs > 0) {
+                    productsAdded++;
+                    System.out.println("Product " + productCount + " added: \"" + productProperties[25] + "\"");
+                }
+                else {
+                    productsSkipped++;
+                    System.out.println("WARNING: Product not added: " + productProperties[25]);
+                }
+                rs = stmt.executeUpdate("INSERT INTO PRODUCTS_CAT (PRODUCT) VALUES (\"" + productId + "\")");
+                if (rs > 0) {
+                    System.out.println("Product added to catalog.");
+                }
+                else {
+                    System.out.println("Product not added to catalog.");
+                }
             }
-            else {
+            catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+                System.out.println("Query: " + SQLQuery);
                 productsSkipped++;
-                System.out.println("WARNING: Product not added: " + productProperties[25]);
             }
-            rs = stmt.executeUpdate("INSERT INTO PRODUCTS_CAT (PRODUCT) VALUES (\"" + productId + "\")");
-        }
-        catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-            System.out.println("Query: " + SQLQuery);
-        }
-        finally {
-            productCount++;
+            finally {
+                productCount++;
+            }
         }
     }
 }
